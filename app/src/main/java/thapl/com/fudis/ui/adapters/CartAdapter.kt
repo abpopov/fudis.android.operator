ca67d38fe6d7554a9139d921484739ebc7c98714
Pavel <p.kolosov@list.ru>
2025-01-14 10:25:51 +0300

    re init

--- app/src/main/java/thapl/com/fudis/ui/adapters/CartAdapter.kt
+++ app/src/main/java/thapl/com/fudis/ui/adapters/CartAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import thapl.com.fudis.GlideRequests
 import thapl.com.fudis.R
